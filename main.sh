7cd3692b6c817a16c746f8bf684acfd3056ebb44
moohr <moohric@moohric.com>
2024-02-11 18:19:43 +0800

    udp, refractor kernel download links

--- main.sh
+++ main.sh
@@ -14,8 +14,8 @@ _________________________________________________
 install_magictcp_kernel(){
 magictcp_kernel_version="6.1.55-magictcp001"
     apt install -y wget
-    wget "https://alist.moohric.com/d/kernel/linux-headers-${magictcp_kernel_version}_${magictcp_kernel_version}-4_amd64.deb" -O "linux-headers-${magictcp_kernel_version}.deb"
-    wget "https://alist.moohric.com/d/kernel/linux-image-${magictcp_kernel_version}_${magictcp_kernel_version}-4_amd64.deb" -O "linux-image-${magictcp_kernel_version}.deb"
+    wget "https://raw.githubusercontent.com/nexstorm/magicTCP/main/kernel/linux-headers-${magictcp_kernel_version}_${magictcp_kernel_version}-4_amd64.deb" -O "linux-headers-${magictcp_kernel_version}.deb"
+    wget "https://raw.githubusercontent.com/nexstorm/magicTCP/main/kernel/linux-image-${magictcp_kernel_version}_${magictcp_kernel_version}-4_amd64.deb" -O "linux-image-${magictcp_kernel_version}.deb"
     dpkg -i "linux-headers-${magictcp_kernel_version}.deb"
     dpkg -i "linux-image-${magictcp_kernel_version}.deb"
     rm -rf "linux-headers-${magictcp_kernel_version}.deb"
@@ -30,11 +30,9 @@ apply_tcp_optimization(){
     declare -A params=(
         ["net.ipv4.tcp_rmem"]="8192 262144 536870912"
         ["net.ipv4.tcp_wmem"]="8192 262144 536870912"
-        ["net.ipv4.tcp_adv_win_scale"]="1"
         ["net.ipv4.tcp_collapse_max_bytes"]="6291456"
         ["net.ipv4.tcp_notsent_lowat"]="131072"
-        ["net.ipv4.udp_rmem_min"]="16384"
-        ["net.ipv4.udp_wmem_min"]="16384"
+        ["net.ipv4.tcp_adv_win_scale"]="1"
         ["net.core.default_qdisc"]="fq"
         ["net.ipv4.tcp_congestion_control"]="bbr"
         ["net.ipv4.tcp_window_scaling"]="1"
@@ -42,6 +40,15 @@ apply_tcp_optimization(){
         ["net.ipv4.ip_forward"]="1"
         ["net.ipv4.conf.all.forwarding"]="1"
         ["net.ipv4.conf.default.forwarding"]="1"
+        # experimental UDP optimisation
+        ["net.ipv4.udp_rmem_min"]="16384"
+        ["net.ipv4.udp_wmem_min"]="16384"
+        ["net.core.rmem_default"]="26214400"
+        ["net.core.rmem_max"]="26214400"
+        ["net.core.optmem_max"]="65535"
+        ["net.ipv4.udp_mem"]="8192 262144 536870912"
+        ["net.core.netdev_max_backlog"]="30000"
+
     )
 
     cp /etc/sysctl.conf /etc/sysctl.conf.bak
@@ -70,7 +77,7 @@ read -p "Please select :" num
     ;;
   *)
   clear
-    echo -e "${Error}:Please select a valid option [0-99]"
+    echo -e "${Error}:Please select a valid option [1, 2]"
     exit 1
     ;;
   esac
